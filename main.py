12d14d64c6ca870d6cef313052f8d6f134c7577e
ViorelsS <viorelss@icloud.com>
2025-10-09 12:42:38 +0200

    Tipi di dato

--- main.py
+++ main.py
@@ -1,3 +1,23 @@
-messaggio = input("Inserisci qualcosa: ")
+""" 04 Tipi di dati
+   - ottenere il tipo di dato
+   - come viene assegnato il tipo
+   - tipi dati (str, int, float, bool, list, tuple, range, dict, set)"""
 
-print(messaggio)
+X = 5.5
+print(type(X))
+
+X = "ciao"
+print(type(X))
+
+X = False
+print(type(X))
+
+# collezione -> list (non è un array -> l'array è nella libreria numpy)
+X= ["roma", "milano", "torino"]
+print(type(X))
+X.append("padova")
+print(X)
+
+X =range(7)
+print(X)
+print(type(X))

90ca868434bb5ff93bf85aede7fb21d870f8e4e4
ViorelsS <viorelss@icloud.com>
2025-10-16 09:41:51 +0200

    Liste: sorthand

--- list.py
+++ list.py
@@ -1,5 +1,26 @@
 # List comprehension
 
-lista_citta = ["udine", "roma", "napoli", "venezia"]
+lista_citta = ["udine", "roma", "bari","napoli", "venezia"]
 
 [print(citta) for citta in lista_citta if citta!="roma"]
+
+x = ["Milano" for citta in lista_citta if citta!="roma"]
+print(x)
+
+
+lista_citta.sort() # Ordinare alfabeticamente la lsita
+print("Lista ordinata", lista_citta)
+lista_citta.sort(reverse=True)
+print("Lista ordinata inversa", lista_citta)
+
+y = list(lista_citta)
+print(y)
+
+for citta in y:
+    x.append(citta)
+    
+print(x)
+
+lista_citta.append(x)
+print(lista_citta)
+
